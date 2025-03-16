1fdfb9b30e9c8cce65841608462ee5a3e87047d8
Kasper Bakker <2305440bakker@zuyd.nl>
2025-03-16 19:06:00 +0100

    Toegevoegd dat je users kunt verwijderen

--- TurfApp/MVVM/View/UsersPage.xaml.cs
+++ TurfApp/MVVM/View/UsersPage.xaml.cs
@@ -1,9 +1,49 @@
-namespace TurfApp.MVVM.View;
+using TurfApp.MVVM.Model;
+using TurfApp.MVVM.Data;
+using System.Collections.ObjectModel;
 
-public partial class UsersPage : ContentPage
+namespace TurfApp.MVVM.View
 {
-	public UsersPage()
+	public partial class UsersPage : ContentPage
 	{
-		InitializeComponent();
+		private readonly Constants _database;
+		public ObservableCollection<User> Users { get; set; } = new();
+
+		public UsersPage()
+		{
+			InitializeComponent();
+			_database = App.Database;
+			BindingContext = this;
+			LoadUsers();
+		}
+
+		private async void LoadUsers()
+		{
+			var users = await _database.GetAllAsync<User>();
+			MainThread.BeginInvokeOnMainThread(() =>
+			{
+				Users.Clear();
+				foreach (var user in users)
+				{
+					Users.Add(user);
+				}
+			});
+		}
+
+		private async void OnDeleteUser(object sender, EventArgs e)
+		{
+			var button = (Button)sender;
+			int userId = (int)button.CommandParameter;
+
+			bool confirm = await DisplayAlert("Verwijderen", "Weet u zeker dat u deze gebruiker wilt verwijderen?", "Ja", "Nee");
+			if (!confirm) return;
+
+			var user = await _database.GetAsync<User>(userId);
+			if (user != null)
+			{
+				await _database.DeleteAsync(user);
+				LoadUsers();
+			}
+		}
 	}
 }
